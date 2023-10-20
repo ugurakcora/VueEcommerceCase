8314588611b099cf7d046daa389602d8518a7c91
ugurakcora <uakcora97@gmail.com>
2023-10-20 07:18:18 +0300

    added counter

--- src/components/ProductDetail.vue
+++ src/components/ProductDetail.vue
@@ -8,6 +8,11 @@
         <h3>{{ product.description }}</h3>
         <p>Fiyat: {{ product.price }} TL</p>
         <p>Stok: {{ product.stock }}</p>
+        <div class="product-quantity" v-if="product.stock > 0">
+          <button @click="decrementQuantity">-</button>
+          <span>{{ product.quantity }}</span>
+          <button @click="incrementQuantity">+</button>
+        </div>
         <button @click="addToCart" :disabled="product.stock === 0">Sepete Ekle</button>
       </div>
       <div class="product-detail-image" v-if="product">
@@ -35,6 +40,7 @@ export default {
     axios.get(`https://dummyjson.com/products/${productId}`)
       .then((response) => {
         this.product = response.data;
+        this.product.quantity = 1; // Adet sayısını başlangıçta 1 olarak ayarla
       })
       .catch((error) => {
         console.error('Ürün detayını alırken hata oluştu:', error);
@@ -42,9 +48,22 @@ export default {
   },
   methods: {
     addToCart() {
-      if (this.product && this.product.stock > 0) {
-        this.$root.$emit('addToCart', this.product);
-        this.product.stock -= 1;
+      if (this.product && this.product.stock > 0 && this.product.quantity > 0) {
+        for (let i = 0; i < this.product.quantity; i++) {
+          this.$root.$emit('addToCart', this.product);
+          this.product.stock -= 1;
+        }
+        this.product.quantity = 1; // Eklenen ürün sayısını sıfırla
+      }
+    },
+    decrementQuantity() {
+      if (this.product.quantity > 1) {
+        this.product.quantity--;
+      }
+    },
+    incrementQuantity() {
+      if (this.product.quantity < this.product.stock) {
+        this.product.quantity++;
       }
     },
   },
