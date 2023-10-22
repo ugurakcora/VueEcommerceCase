c09b803331167efad273a91ccd605475684e1b48
ugurakcora <uakcora97@gmail.com>
2023-10-22 03:44:12 +0300

    added filter

---
+++ src/components/Categories.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <div v-for="category in categories" :key="category.id">
+      <span>
+        <button @click="filterProductsByCategory(category)" :class="{ active: selectedCategory === category }">
+          {{ category }}
+        </button>
+      </span>
+    </div>
+    <button @click="resetFilter">Tüm Kategorileri Göster</button>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      categories: [],
+      selectedCategory: null
+    };
+  },
+  created() {
+    axios.get('https://dummyjson.com/products/categories')
+      .then((response) => {
+        this.categories = response.data;
+      })
+      .catch((error) => {
+        console.error('Kategorileri alırken hata oluştu:', error);
+      });
+  },
+  methods: {
+    filterProductsByCategory(category) {
+      this.selectedCategory = category;
+      this.$emit('filter-by-category', category);
+    },
+    resetFilter() {
+      this.selectedCategory = null;
+      this.$emit('filter-by-category', null); // Tüm kategoriyi göster
+    }
+  }
+};
+</script>
+
+<style>
+.active {
+  background-color: #3490dc;
+  color: #fff;
+}
+</style>
--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,11 +1,15 @@
 <template>
   <div>
+    <Categories :categories="categories" @filter-by-category="filterProductsByCategory" />
     <h1>Ürün Listesi</h1>
     <div class="product">
-      <div class="product_item" v-for="product in products" :key="product.id">
+      <div v-if="filteredProducts.length > 0" class="product_item" v-for="product in filteredProducts" :key="product.id">
         <a class="product_item-wrapper" :href="`/product/${product.id}`">{{ product.title }}
           <img class="product_item-img" :src="product.images[0]" :alt="product.title" />
         </a>
+      </div>
+      <div v-else>
+        <p>Seçilen kategoriye ait ürün bulunmamaktadır.</p>
       </div>
     </div>
   </div>
@@ -13,20 +17,36 @@
 
 <script>
 import axios from 'axios';
+import Categories from './Categories.vue';
 
 export default {
   components: {
+    Categories,
   },
   data() {
     return {
       products: [],
+      categories: [],
+      selectedCategory: null,
     };
   },
+  computed: {
+    filteredProducts() {
+      if (!this.selectedCategory) {
+        return this.products;
+      }
+      return this.products.filter(product => product.category === this.selectedCategory);
+    },
+  },
+  methods: {
+    filterProductsByCategory(category) {
+      this.selectedCategory = category;
+    },
+  },
   created() {
-    axios.get('https://dummyjson.com/products') // Ürün verilerini almak için API isteği
+    axios.get('https://dummyjson.com/products')
       .then((response) => {
-        this.products = response.data.products; // API yanıtından gelen verileri kullanın
-        console.log(response.data.products)
+        this.products = response.data.products;
       })
       .catch((error) => {
         console.error('Ürün verilerini alırken hata oluştu:', error);
